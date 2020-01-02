12ba339625f3678a8961cc4260086301ae46dd4d
VictorTellez219 <vyctortellez@gmail.com>
2020-01-02 14:02:32 -0500

    Small fixes, in UI and variable type

--- RecipeBook/src/app/shared/ingredient.model.ts
+++ RecipeBook/src/app/shared/ingredient.model.ts
@@ -2,8 +2,9 @@ export class Ingredient {
   public name: string;
   public amount: number;
 
-  constructor(public name: string, public amount: string){
-
+  constructor(name: string, amount: number){
+    this.name = name;
+    this.amount = amount;
   }
 
 }
